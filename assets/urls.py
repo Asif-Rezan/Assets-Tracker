0d0235f0abe7118306d034a2d5df6abae5f8904a
REZAN <asifrezan@gmail.com>
2023-03-04 16:14:21 +0600

    Add company completed

--- assets/urls.py
+++ assets/urls.py
@@ -1,3 +1,7 @@
+from django.urls import path
+from .views import AddCompany
+
+
 urlpatterns = [
-    
+     path("add-company/", AddCompany,name="add-company"),
 ]
--- assets/views.py
+++ assets/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from django.contrib.auth.hashers import make_password
+from .models import Company
 
-# Create your views here.
+
+#Sign Up as a company
+@api_view(['POST'])
+def AddCompany(request):
+    name = request.POST.get("name")
+    address = request.POST.get("address")
+    email = request.POST.get("email")
+    password = request.POST.get("password")
+   
+    hashed_password = make_password(password)
+
+    company = Company(
+        name=name,
+        address=address,
+        email=email,
+        password = hashed_password
+    )
+    company.save()
+
+    return Response("Company added successfully")
